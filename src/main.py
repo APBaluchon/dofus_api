67b225ef259088e51b19b216f1d1f276fd313684
APBaluchon <aurelien-petitfrere@laposte.net>
2024-03-05 22:51:17 +0100

    update

--- src/main.py
+++ src/main.py
@@ -4,29 +4,9 @@ from Object.RessourceObject import RessourceObject
 from Scraper.EntityScraper import EntityScraper
 from DAO.filldb import FillDB
 from Utils.utils import get_number_pages, get_all_links_from_page, get_all_links
-import time
+from DAO.ressources_dao import RessourcesDao
 
 if __name__ == "__main__":
-    links = open("src/links/ressources_links.txt", "r").read().split("\n")
-    links.pop()
-    db = FillDB("dofusdb", "ressources", os.getenv("password"))
-    # number_of_created = 0
-    # for i, link in enumerate(links):
-    #     if (number_of_created+1) % 50 == 0:
-    #         time.sleep(60)
-    #     print(f"{i+1}/{len(links)}")
-
-    #     id = link.split("/")[-1].split("-")[0]
-
-    #     if not db.id_exists(id):
-    #         ressource = RessourceObject(link)
-    #         try:
-    #             db.insert(ressource)
-    #             number_of_created += 1
-    #         except:
-    #             print(f"Error with {ressource.name}")
-    #             continue
-    item_1 = RessourceObject("https://www.dofus.com/fr/mmorpg/encyclopedie/ressources/21966-rune-astrale-majeure")
-    db.replace(item_1)
-    db.close()
+    db = RessourcesDao()
+    print(db.get_ressource_by_id(21966))
 
