f7ad886d0ca2df5bdf417d450502630a1fa93c80
APBaluchon <aurelien-petitfrere@laposte.net>
2024-03-06 21:33:13 +0100

    up

--- src/app.py
+++ src/app.py
@@ -11,6 +11,14 @@ async def get_all_ressources(limit: int = 10000):
 async def get_ressource_by_id(id: int):
     return ObjectDao().get_object_by_id(id)[0]
 
+@app.get("/consommables/{id}")
+async def get_consommable_by_id(id: int):
+    return ObjectDao("consommables").get_object_by_id(id)[0]
+
+@app.get("/consommables/")
+async def get_all_consommables(limit: int = 10000):
+    return ObjectDao("consommables").get_all_objects(limit)
+
 if __name__ == "__main__":
     import uvicorn
 
