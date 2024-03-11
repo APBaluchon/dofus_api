0682374a6a50d0eecab4116a5a4d12ea492ad549
Yanis Colléaux <yaniscolleaux223@gmail.com>
2024-03-11 15:11:03 +0100

    test filtering + license

--- src/test/test_dao.py
+++ src/test/test_dao.py
@@ -20,10 +20,18 @@ class TestObjectDAO(unittest.TestCase):
         self.assertEqual(dao.get_object_by_id(ressource_id), [])
 
     def test_read_all_ok(self):
-
         dao = ObjectDao("ressources")
 
         self.assertIsNotNone(dao.get_all_objects(limit=3))
+
+    def test_read_all_filtering_ok(self):
+        level = 200
+
+        dao = ObjectDao("ressources")
+        read_filtered = dao.get_all_objects(level = level)
+
+        self.assertNotEqual(dao.get_all_objects(), read_filtered)
+        self.assertIsNotNone(read_filtered)
 
 
 if __name__ == "__main__":
