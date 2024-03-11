2cfe1e9e8c658f725b3c5cfc4ceb4bd691126f8c
APBaluchon <aurelien-petitfrere@laposte.net>
2024-03-11 10:32:48 +0100

    update drago

--- src/main.py
+++ src/main.py
@@ -5,9 +5,5 @@ from DAO.DB import DB
 
 if __name__ == "__main__":
     db = DB("dofusdb")
-    link = open("src/links/link_montures.txt", "r").read().split("\n")
-    for l in link:
-        monture = MontureObject(l)
-        db.insert_entity("montures", monture)
-        sleep(120)
+    db.fill("montures")
         
